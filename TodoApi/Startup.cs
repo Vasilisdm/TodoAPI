9bd5871465fa6ce3a5268468aa61f02bf068d804
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-11 20:01:18 +0200

    Removed swagger calls.

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -30,10 +30,7 @@ namespace TodoApi
         {
             services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApi", Version = "v1" });
-            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,8 +39,6 @@ namespace TodoApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
             }
 
             app.UseHttpsRedirection();
